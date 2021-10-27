b102427a42e4be7d8d46d0313d91063de56dbaa3
caner <caner@quizco.de>
2021-10-27 17:26:40 +0200

    long  statements shortened

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -172,7 +172,14 @@ const Header = (props) => {
   const routes = useMemo(
     () => [
       { name: "Home", link: "/", activeIndex: 0 },
-      { name: "Services", link: "/services", activeIndex: 1 },
+      {
+        name: "Services",
+        link: "/services",
+        activeIndex: 1,
+        ariaOwns: anchorEl ? "simple-menu" : undefined,
+        ariaHaspopup: anchorEl ? true : undefined,
+        onMouseOver: (event) => handleClick(event),
+      },
       { name: "The Revolution", link: "/revolution", activeIndex: 2 },
       { name: "About Us", link: "/about", activeIndex: 3 },
       { name: "Contact Us", link: "/contact", activeIndex: 4 },
@@ -181,20 +188,6 @@ const Header = (props) => {
     []
   );
   useEffect(() => {
-    // if (window.location.pathname === "/" && value !== 0) {
-    //   setValue(0);
-    // } else if (window.location.pathname === "/services" && value !== 1) {
-    //   setValue(1);
-    // } else if (window.location.pathname === "/revolution" && value !== 2) {
-    //   setValue(2);
-    // } else if (window.location.pathname === "/about" && value !== 3) {
-    //   setValue(3);
-    // } else if (window.location.pathname === "/contact" && value !== 4) {
-    //   setValue(4);
-    // } else if (window.location.pathname === "/estimate" && value !== 5) {
-    //   setValue(5);
-    // }
-
     [...menuItems, ...routes].forEach((route) => {
       switch (window.location.pathname) {
         case `${route.link}`:
@@ -274,7 +267,18 @@ const Header = (props) => {
         className={classes.tabContainer}
         indicatorColor="primary"
       >
-        <Tab className={classes.tab} component={Link} to="/" label="Home"></Tab>
+        {routes.map((route, index) => (
+          <Tab
+            aria-owns={route.ariaOwns}
+            aria-haspopup={route.ariaHaspopup}
+            onMouseOver={route.onMouseOver}
+            className={classes.tab}
+            component={Link}
+            to={route.link}
+            label={route.name}
+          />
+        ))}
+        {/* <Tab className={classes.tab} component={Link} to="/" label="Home"></Tab>
         <Tab
           aria-owns={anchorEl ? "simple-menu" : undefined}
           aria-haspopup={anchorEl ? true : undefined}
@@ -301,7 +305,7 @@ const Header = (props) => {
           component={Link}
           to="/contact"
           label="Contact Us"
-        ></Tab>
+        ></Tab>*/}
       </Tabs>{" "}
       <Button variant="contained" color="secondary" className={classes.button}>
         Free Estimate
